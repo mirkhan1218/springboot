24f4d6cdf80292ce755f54b89735ecb868509424
mirkhan <mirkhan1218@gmail.com>
2023-06-16 04:17:10 +0900

    프로젝트 생성

---
+++ src/main/java/hotkick/babylog/BabylogApplication.java
@@ -0,0 +1,13 @@
+package hotkick.babylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabylogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabylogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hotkick/babylog/BabylogApplicationTests.java
@@ -0,0 +1,13 @@
+package hotkick.babylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabylogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
